0841b681f8910512d07402678d6e787da886c600
Ed Brainerd <56053069+lbrainrd@users.noreply.github.com>
2022-11-18 20:22:30 +0000

    first exercise

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -6,8 +6,17 @@ package com.linkedin.javacodechallenges;
  */
 public class App 
 {
+    public static boolean isEven(int n) {
+        if (n % 2 > 0) {
+            return false;
+        } else {
+            return true;
+        }
+        
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(isEven(2));
     }
 }
